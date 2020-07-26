de5dcfb3fdcf05d575241321ded0e558a1586441
Christopher DiStasio <cadistasio@gmail.com>
2020-07-25 21:39:16 -0500

    Stubs fields

--- banking-api/src/main/java/com/bankingexamples/models/User.java
+++ banking-api/src/main/java/com/bankingexamples/models/User.java
@@ -2,4 +2,13 @@ package com.bankingexamples.models;
 
 public class User {
 
+	private int userId; // primary key
+	private String username; // not null, unique
+	private String password; // not null
+	private String firstName; // not null
+	private String lastName; // not null
+	private String email; // not null
+	private Role role;
+	
 }
+
