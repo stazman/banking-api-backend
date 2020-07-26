147239ada47f78c6728b86222112e4d299c6a2b3
Christopher DiStasio <cadistasio@gmail.com>
2020-07-25 21:41:20 -0500

    Adds, stubs fields for Account model

---
+++ banking-api/src/main/java/com/bankingexamples/models/Account.java
@@ -0,0 +1,11 @@
+package com.bankingexamples.models;
+
+public class Account {
+	  
+	private int accountId; // primary key
+	private double balance;  // not null
+	private AccountStatus status;
+	private AccountType type;
+	
+}
+
