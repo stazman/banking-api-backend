b7639b299dbcac07005b85c7f05321988017ffa0
Christopher DiStasio <cadistasio@gmail.com>
2020-07-27 16:25:46 -0500

    Stubs constructor, getters, setters for Role bean

--- banking-api/src/main/java/com/bankingexamples/models/Role.java
+++ banking-api/src/main/java/com/bankingexamples/models/Role.java
@@ -4,6 +4,32 @@ public class Role {
 	  
 	private int roleId; // primary key
 	private String role; // not null, unique
+	
+	
+	public Role() {
+		this.roleId = roleId;
+		this.role = role;
+	}
+
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
 
