ac21df80c389d772c3bae33e714ff7151cd4d829
Christopher DiStasio <cadistasio@gmail.com>
2020-07-27 16:30:37 -0500

    Stubs constructor, getters, setters for AccountStatus bean

--- banking-api/src/main/java/com/bankingexamples/models/AccountStatus.java
+++ banking-api/src/main/java/com/bankingexamples/models/AccountStatus.java
@@ -4,6 +4,34 @@ public class AccountStatus {
 	
 	private int statusId; // primary key
 	private String status; // not null, unique
+	
+	
+	public AccountStatus() {
+
+		this.statusId = statusId;
+		this.status = status;
+		
+	}
+
+
+	public int getStatusId() {
+		return statusId;
+	}
+
+
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
 
