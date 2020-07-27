3a0978825b28dd0eb9a5a71404466286ce7716d0
Christopher DiStasio <cadistasio@gmail.com>
2020-07-27 16:27:56 -0500

    Stubs constructor, getters, setters for AccountType bean

--- banking-api/src/main/java/com/bankingexamples/models/AccountType.java
+++ banking-api/src/main/java/com/bankingexamples/models/AccountType.java
@@ -5,5 +5,27 @@ public class AccountType {
 	private int typeId; // primary key
 	private String type; // not null, unique
 	
+	public AccountType() {
+
+		this.typeId = typeId;
+		this.type = type;
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
 
