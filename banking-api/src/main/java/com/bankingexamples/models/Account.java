440802265d6b7089fc5c5a76e05f65d87a9c8913
Christopher DiStasio <cadistasio@gmail.com>
2020-07-27 16:29:45 -0500

    Stubs constructor, getters, setters for Account bean

--- banking-api/src/main/java/com/bankingexamples/models/Account.java
+++ banking-api/src/main/java/com/bankingexamples/models/Account.java
@@ -7,5 +7,44 @@ public class Account {
 	private AccountStatus status;
 	private AccountType type;
 	
+	
+	public Account() {
+		
+		this.accountId = accountId;
+		this.balance = balance;
+		this.status = status;
+		this.type = type;
+	}
+
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+
+	public AccountStatus getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(AccountStatus status) {
+		this.status = status;
+	}
+
+
+	public AccountType getType() {
+		return type;
+	}
+
+
+	public void setType(AccountType type) {
+		this.type = type;
+	}
+	
 }
 
