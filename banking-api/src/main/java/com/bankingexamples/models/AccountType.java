2e1a9d5461aba226d48556cfceb2be3f1229c0ca
Christopher DiStasio <cadistasio@gmail.com>
2020-07-28 14:38:39 -0500

    Assigns default values to AccountType constructors

--- banking-api/src/main/java/com/bankingexamples/models/AccountType.java
+++ banking-api/src/main/java/com/bankingexamples/models/AccountType.java
@@ -5,23 +5,29 @@ public class AccountType {
 	private int typeId; // primary key
 	private String type; // not null, unique
 	
+	
 	public AccountType() {
 
-		this.typeId = typeId;
-		this.type = type;
+		this.typeId = 0;
+		this.type = null;
+		
 	}
+	
 
 	public int getTypeId() {
 		return typeId;
 	}
+	
 
 	public void setTypeId(int typeId) {
 		this.typeId = typeId;
 	}
+	
 
 	public String getType() {
 		return type;
 	}
+	
 
 	public void setType(String type) {
 		this.type = type;
