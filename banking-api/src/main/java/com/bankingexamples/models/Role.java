0db03bb66c0f339b28595f62588c13023fd56a9b
Christopher DiStasio <cadistasio@gmail.com>
2020-07-29 20:03:33 -0500

    Generates overriding toString(), equals(), hashCode() for Role

--- banking-api/src/main/java/com/bankingexamples/models/Role.java
+++ banking-api/src/main/java/com/bankingexamples/models/Role.java
@@ -31,5 +31,42 @@ public class Role {
 		this.role = role;
 	}
 
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + roleId;
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [roleId=" + roleId + ", role=" + role + "]";
+	}
+	
 }
+
+
 
