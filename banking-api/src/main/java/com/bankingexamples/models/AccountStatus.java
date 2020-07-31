7a8952b36efffde43ff6dd3e9c5104e96b9513c7
Christopher DiStasio <cadistasio@gmail.com>
2020-07-30 19:40:21 -0500

    toString(), equals(), hashCode() for AccountStatus

--- banking-api/src/main/java/com/bankingexamples/models/AccountStatus.java
+++ banking-api/src/main/java/com/bankingexamples/models/AccountStatus.java
@@ -33,5 +33,41 @@ public class AccountStatus {
 		this.status = status;
 	}
 
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + statusId;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountStatus other = (AccountStatus) obj;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (statusId != other.statusId)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AccountStatus [statusId=" + statusId + ", status=" + status + "]";
+	}
+	
 }
 
