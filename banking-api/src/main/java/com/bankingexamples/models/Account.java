b909efe0fae1f892bee3eeee9d9f23bc94684229
Christopher DiStasio <cadistasio@gmail.com>
2020-07-30 19:39:23 -0500

    Generates overridden toString(), equals(), hashCode() for Role

--- banking-api/src/main/java/com/bankingexamples/models/Account.java
+++ banking-api/src/main/java/com/bankingexamples/models/Account.java
@@ -55,6 +55,54 @@ public class Account {
 	public void setType(AccountType type) {
 		this.type = type;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + accountId;
+		long temp;
+		temp = Double.doubleToLongBits(balance);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountId != other.accountId)
+			return false;
+		if (Double.doubleToLongBits(balance) != Double.doubleToLongBits(other.balance))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Account [accountId=" + accountId + ", balance=" + balance + ", status=" + status + ", type=" + type
+				+ "]";
+	}
 	
 }
 
