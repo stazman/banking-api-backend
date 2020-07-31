d8e316bc17cd250f8fc9826341935de7b2140aac
Christopher DiStasio <cadistasio@gmail.com>
2020-07-30 19:40:44 -0500

    Generates overridden toString(), equals(), hashCode() for AccountType

--- banking-api/src/main/java/com/bankingexamples/models/AccountType.java
+++ banking-api/src/main/java/com/bankingexamples/models/AccountType.java
@@ -32,6 +32,42 @@ public class AccountType {
 	public void setType(String type) {
 		this.type = type;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + typeId;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountType other = (AccountType) obj;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (typeId != other.typeId)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AccountType [typeId=" + typeId + ", type=" + type + "]";
+	}
 	
 }
 
