5306360307956ba5901363d564e305e9581ebfd7
Christopher DiStasio <cadistasio@gmail.com>
2020-08-04 20:57:51 -0500

    Stubs AccountDAO

--- banking-api/src/main/java/com/bankingexamples/dao/AccountDAO.java
+++ banking-api/src/main/java/com/bankingexamples/dao/AccountDAO.java
@@ -1,5 +1,20 @@
 package com.bankingexamples.dao;
 
+import java.util.Set;
+
+import com.bankingexamples.models.Account;
+import com.bankingexamples.models.AccountStatus;
+import com.bankingexamples.models.AccountType;
+
 public interface AccountDAO {
+
+	public Integer createAccount(int id);
+	public Account getAccountById(int id);
+	public Set<Account> getAccountsByStatus(AccountStatus status);
+	public Set<Account> getAccountsByType(AccountType type);
+	public Account makeDeposit(int id);
+	public Account makeWithdrawal(int id);
+	public Account makeTransfer(int id);
+	public void updateAccount(int id);
 
 }
