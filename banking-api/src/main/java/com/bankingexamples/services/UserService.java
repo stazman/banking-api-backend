30a6557f98ff47fdc75e5dc4c6c7da89e7dafcb4
Christopher DiStasio <cadistasio@gmail.com>
2020-08-04 21:53:20 -0500

    Adds, stubs UserService

---
+++ banking-api/src/main/java/com/bankingexamples/services/UserService.java
@@ -0,0 +1,22 @@
+package com.bankingexamples.services;
+
+public class UserService {
+
+	//not register
+	//login
+	//can update their own info if the id provided matches the id of the current user
+	
+	//owned accounts:
+		//withdrawal
+		//deposit
+		//transfer between all accounts of user
+		//transfer between accounts owned by different users ... if the source account belongs to the current user
+		//can find accounts if the account belongs to the current user
+		//can find accounts by user if the id provided matches the id of the current user ???
+		//can submit an account 
+		//if the account belongs to the current user
+	
+}
+
+
+
