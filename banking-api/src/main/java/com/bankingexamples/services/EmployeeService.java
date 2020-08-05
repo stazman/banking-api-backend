c3c0e16b96d33453bfc1b3596bbbdd1647675491
Christopher DiStasio <cadistasio@gmail.com>
2020-08-04 21:54:13 -0500

    Adds, stubs EmployeeService

---
+++ banking-api/src/main/java/com/bankingexamples/services/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.bankingexamples.services;
+
+public class EmployeeService {
+
+	//has standard user account
+	//has additional abilities
+	
+		//can view all customer information ... even admins???
+		//can find all users ???
+		//can find a user by id
+		//can find accounts ???
+		//can find accounts by id
+		//can find accounts by user ???
+		//can find accounts by status
+		//can submit an account
+		
+		
+	
+}
