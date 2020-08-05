9caa830fa2bd1e670e5c475956223f399f89c58d
Christopher DiStasio <cadistasio@gmail.com>
2020-08-04 21:53:52 -0500

    Adds, stubs AdminService

---
+++ banking-api/src/main/java/com/bankingexamples/services/AdminService.java
@@ -0,0 +1,23 @@
+package com.bankingexamples.services;
+
+public class AdminService {
+	
+	//register users
+	//add special abilities to standard user
+	
+		//can view all users
+		//can directly modify/update all users
+		//can withdraw from any user account
+		//can deposit for any user
+		//admin can transfer
+	
+		//can find users ???
+		//can find users by id
+		//can update users
+		//can find accounts ???
+		//can find accounts by id
+		//can find accounts by status
+		//can submit an account
+		//can update an account
+		
+}
