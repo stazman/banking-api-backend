88b167888ba789d14cb5d379929340aead025885
Christopher DiStasio <cadistasio@gmail.com>
2020-08-06 17:58:51 -0500

    Stubs AccountPostgres

---
+++ banking-api/src/main/java/com/bankingexamples/dao/AccountPostgres.java
@@ -0,0 +1,39 @@
+package com.bankingexamples.dao;
+
+import java.util.Set;
+
+import com.bankingexamples.models.Account;
+
+public class AccountPostgres implements AccountDAO {
+
+	@Override
+	public Integer createAccount(Integer userId, Account acct) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double getAccountBalanceByAcctId(Integer acctId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer updateAccount(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Account> getAccountsByStatus(Account status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Account> getAccountsByUserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
