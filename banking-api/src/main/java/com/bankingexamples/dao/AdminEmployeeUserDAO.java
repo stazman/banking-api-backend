6daea0643351b691a9fc52b926154221f45d6484
Christopher DiStasio <cadistasio@gmail.com>
2020-08-06 16:26:00 -0500

    Stubs AdminEmployeeUserDAO

---
+++ banking-api/src/main/java/com/bankingexamples/dao/AdminEmployeeUserDAO.java
@@ -0,0 +1,11 @@
+package com.bankingexamples.dao;
+
+import java.util.Set;
+
+import com.bankingexamples.models.User;
+
+public interface AdminEmployeeUserDAO {
+
+	public Set<User> getAllUsers();
+
+}
