0ce64c43cc5bc58b2e42d6bbf9632c015273b129
Christopher DiStasio <cadistasio@gmail.com>
2020-08-09 11:29:56 -0500

    Adds, stubs AdminDelegate

---
+++ banking-api/src/main/java/com/bankingexamples/delegates/AdminDelegate.java
@@ -0,0 +1,19 @@
+package com.bankingexamples.delegates;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminDelegate implements FrontControllerDelegate{
+
+	@Override
+	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+} 
