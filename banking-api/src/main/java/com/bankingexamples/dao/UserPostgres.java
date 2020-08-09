f8337154c21960978a173fecb057cdafc8b97635
Christopher DiStasio <cadistasio@gmail.com>
2020-08-09 12:23:08 -0500

    Completes UserPostgres

--- banking-api/src/main/java/com/bankingexamples/dao/UserPostgres.java
+++ banking-api/src/main/java/com/bankingexamples/dao/UserPostgres.java
@@ -3,7 +3,7 @@ package com.bankingexamples.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,30 +13,10 @@ import com.bankingexamples.utilities.ConnectionUtil;
 
 public class UserPostgres implements UserDAO {
 
-	
-	public static void main(String[] args) {
-		
-//		UserPostgres up = new UserPostgres();
-//		
-//		User u = up.getUserById(1);
-//		
-//		System.out.println(u);		
-		
-//		UserPostgres up = new UserPostgres();
-////		
-//		User u = up.registerUser("a", "a", "a", "a", "a", "Standard");
-////		
-//		System.out.println(u);		
-		
-		
-	}
-	
 	ConnectionUtil cu = ConnectionUtil.getConnectionUtil();
 
 	@Override
-	public Integer registerUser(User u) {
-		
-		Integer id = 0;
+	public User registerUser(User u) {
 		
 		try (Connection conn = cu.getConnection()) {
 			
@@ -57,21 +37,23 @@ public class UserPostgres implements UserDAO {
 			
 			pstmt.executeUpdate();
 			
-			ResultSet rs = pstmt.getGeneratedKeys();
+			System.out.println(pstmt);
 			
-			if (rs.next()) {
-				id = rs.getInt(1);
-				conn.commit();
-			} else {
-				conn.rollback();
+			ResultSet rs = pstmt.getGeneratedKeys();
+
+			if(rs.next()) {
+				u.setUserId(rs.getInt(1)); 
+				return u;
 			}
-		} catch (Exception e) {
+		
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		
-		return id;
+		return null;
+		
 	}
-
+	
 	public User getUserById(int id) {
 		
 		User u = null;
@@ -112,7 +94,81 @@ public class UserPostgres implements UserDAO {
 	}
 
 	@Override
-	public boolean updateUser(User u) {
+	public User getUserByUsername(String username) {
+		
+		try (Connection conn = cu.getConnection()) {
+
+			String sql = "select * from bank_user join role on bank_user.role_id = role.role_id where user_name = ?";
+
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, username);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				User u = new User();
+				u.setUserId(rs.getInt("persn_id"));
+				u.setPassword(rs.getString("passwd"));
+				u.setFirstName(rs.getString("fst_nm"));
+				u.setLastName(rs.getString("lst_nm"));
+				u.setEmail(rs.getString("eml"));
+
+				
+				Role r = new Role();
+				r.setRoleId(rs.getInt("bank_role_id"));
+				r.setRole(rs.getString("bank_role"));
+				u.setRole(r);
+				
+				return u;
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public Set<User> getAllUsers() {
+		Set<User> users = new HashSet<>();
+
+		try {
+			Connection conn = cu.getConnection();
+			String sql = "select user_id, user_name, first_name, last_name, email, role.role_id, role.role_name from "
+			+ "bank_user join role on bank_user.role_id = role.role_id";		
+			Statement stmt = conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+
+				User u = new User();
+
+				u.setUserId(rs.getInt("id"));
+				u.setUsername(rs.getString("usernm"));
+				u.setFirstName(rs.getString("fst_nm"));
+				u.setLastName(rs.getString("lst_nm"));
+				u.setEmail(rs.getString("eml"));
+				
+				Role role = new Role();
+				role.setRoleId(rs.getInt("bank_role_id"));
+				role.setRole(rs.getString("bank_role"));
+				u.setRole(role);
+				
+				users.add(u);
+				
+			}
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return users;
+	}
+	
+	@Override
+	public User updateUser(User u) {
 		
 		try (Connection conn = cu.getConnection()) {
 			
@@ -129,102 +185,20 @@ public class UserPostgres implements UserDAO {
 				pstmt.setString(4, u.getLastName());
 				pstmt.setString(5, u.getEmail());
 				pstmt.setInt(6, u.getRole().getRoleId());
+				pstmt.setInt(7,  u.getUserId());
 				
-				int rowsAffected = pstmt.executeUpdate();
+				int rs = pstmt.executeUpdate();
 				
-				if (rowsAffected > 0) {
-			
-					conn.commit();
-					
-					return true;
-			
-				} else {
-					
-					conn.rollback();
-					
+				if(rs > 0) {
+					return u;
 				}
 				
-		} catch (Exception e) {
+				
+			} catch(Exception e) {
 				e.printStackTrace();
-		}
-		
-		return false;
-	}
-
-	@Override
-	public User getUserByUsername(String username) {
-		
-		User u = null;
-		
-		try (Connection conn = cu.getConnection()) {
-			String sql = "select persn.id, persn.usernm, persn.passwd, "
-					+ "bank_role_id, bank_role.bank_role as "
-					+ "bank_role from persn join bank_role on "
-					+ "persn.id = bank_role.id where usernm = ? and "
-					+ "passwd = ?";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, username);
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			if (rs.next()) {
-				u = new User();
-				u.setUserId(rs.getInt("id"));
-				u.setPassword(rs.getString("passwd"));
-				u.setUsername(rs.getString("usernm"));
-				u.setFirstName(rs.getString("fst_nm"));
-				u.setLastName(rs.getString("lst_nm"));
-				u.setEmail(rs.getString("eml"));
-
-				
-				Role r = new Role();
-				r.setRoleId(rs.getInt("bank_role_id"));
-				r.setRole(rs.getString("bank_role"));
-				u.setRole(r);
-				
 			}
 			
-		} catch (Exception e) {
-			e.printStackTrace();
+			return null;
 		}
-		
-		return u;
-	}
-
-	@Override
-	public Set<User> getAllUsers() {
-		Set<User> users = new HashSet<>();
-
-		try {
-			Connection conn = cu.getConnection();
-			String sql = "SELECT * FROM Users";
-			
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-
-			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-
-				User u = new User();
-
-				u.setUserId(rs.getInt("id"));
-				u.setPassword(rs.getString("passwd"));
-				u.setUsername(rs.getString("usernm"));
-				u.setFirstName(rs.getString("fst_nm"));
-				u.setLastName(rs.getString("lst_nm"));
-				u.setEmail(rs.getString("eml"));
-				
-				users.add(u);
-				
-			}
-			
-			rs.close();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 
-
-		return users;
-	}
 
 }
