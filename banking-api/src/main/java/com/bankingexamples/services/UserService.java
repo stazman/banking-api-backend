1b3a921c6fc2e40e36759fe20c64abaa4e4222b0
Christopher DiStasio <cadistasio@gmail.com>
2020-08-11 11:51:04 -0500

    Adds findUserByUsername(); removes findAllUsers()

--- banking-api/src/main/java/com/bankingexamples/services/UserService.java
+++ banking-api/src/main/java/com/bankingexamples/services/UserService.java
@@ -21,8 +21,8 @@ public class UserService {
 		return userDao.updateUser(u);	
 	}
 	
-	public Set<User> findAllUsers() {
-		return userDao.getAllUsers();
+	public User findUserByUsername(String username) {
+		return userDao.getUserByUsername(username);
 	}
 
 }
