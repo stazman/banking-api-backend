9afa874c47062c657d28a44ddddafd60d1de22a4
Christopher DiStasio <cadistasio@gmail.com>
2020-08-11 11:59:42 -0500

    Adds, stubs UserDelegate

---
+++ banking-api/src/main/java/com/bankingexamples/delegates/UserDelegate.java
@@ -0,0 +1,52 @@
+package com.bankingexamples.delegates;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bankingexamples.dao.UserPostgres;
+import com.bankingexamples.models.User;
+import com.bankingexamples.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UserDelegate implements FrontControllerDelegate {
+
+	private UserService uServ = new UserService(new UserPostgres());
+	
+	private ObjectMapper om = new ObjectMapper();
+	
+	@Override
+	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = (String) req.getAttribute("path");
+		
+		int userId = Integer.valueOf(path);
+		
+		User user = null;
+		
+		switch (req.getMethod()) {		
+		
+			case "GET":
+				user = uServ.findUserById(userId);
+				if (user != null) 
+					resp.getWriter().write(om.writeValueAsString(user));
+				else
+					resp.sendError(404, "User not found.");
+				break;
+				
+			case "PUT":
+				
+				req.getSession().getAttribute("user");
+									
+				user = om.readValue(req.getInputStream(), User.class);
+					uServ.updateUser(user);
+					resp.getWriter().write(om.writeValueAsString(user));
+				
+					resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+				}
+
+	}			
+}
+
