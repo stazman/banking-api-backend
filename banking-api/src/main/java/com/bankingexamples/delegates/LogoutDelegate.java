d068c89b414b08ad0ab04d73b351deb77e6f4cdb
Christopher DiStasio <cadistasio@gmail.com>
2020-08-11 11:30:32 -0500

    Stubs LogoutDelegate

---
+++ banking-api/src/main/java/com/bankingexamples/delegates/LogoutDelegate.java
@@ -0,0 +1,34 @@
+package com.bankingexamples.delegates;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bankingexamples.models.User;
+
+public class LogoutDelegate implements FrontControllerDelegate {
+
+	@Override
+	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		if ("DELETE".equals(req.getMethod())) {
+			
+			if(req.getSession().getAttribute("user") != null) {
+				
+				resp.getWriter().write("You have successfully logged out " + ((User) req.getSession().getAttribute("user")).getUsername());
+				req.getSession().invalidate();
+				
+			} else {
+				
+				resp.sendError(400, "There was no user logged into the session");
+			}
+			
+		}else {
+			
+			resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);}
+
+	}
+
+}
