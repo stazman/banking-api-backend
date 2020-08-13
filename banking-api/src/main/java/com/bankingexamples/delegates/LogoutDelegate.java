5d6c80fd3b4a3baae0093ebdbbbee4c16e82ad1a
Christopher DiStasio <cadistasio@gmail.com>
2020-08-13 16:55:18 -0500

    Adds current user's username to successful log out message

--- banking-api/src/main/java/com/bankingexamples/delegates/LogoutDelegate.java
+++ banking-api/src/main/java/com/bankingexamples/delegates/LogoutDelegate.java
@@ -6,16 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bankingexamples.models.User;
+
 public class LogoutDelegate implements FrontControllerDelegate {
 
 	@Override
 	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		User thisUser = (User) req.getSession().getAttribute("user");
 
 		if ("DELETE".equals(req.getMethod())) {			
 			
 			if(req.getSession().getAttribute("user") != null) {
 				
-				resp.getWriter().write("You have successfully logged out.");
+				resp.getWriter().write("You have successfully logged out, " + thisUser.getUsername() + ".");
 				
 				req.getSession().invalidate();
 				
