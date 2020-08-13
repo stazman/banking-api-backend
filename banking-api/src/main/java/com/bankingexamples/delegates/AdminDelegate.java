c82a28e9d0abbd5842374bee5017dd38e4bfde0e
Christopher DiStasio <cadistasio@gmail.com>
2020-08-12 21:58:27 -0500

    Successfully registers new users with admin login

--- banking-api/src/main/java/com/bankingexamples/delegates/AdminDelegate.java
+++ banking-api/src/main/java/com/bankingexamples/delegates/AdminDelegate.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bankingexamples.dao.UserPostgres;
-import com.bankingexamples.models.Role;
 import com.bankingexamples.models.User;
 import com.bankingexamples.services.AdminService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class AdminDelegate implements FrontControllerDelegate{
 
@@ -22,30 +22,42 @@ public class AdminDelegate implements FrontControllerDelegate{
 	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String path = (String) req.getAttribute("path");
 		
+		
+		System.out.println(path);
+		
+	
 		if (path == null || path.equals("")) {
+		
+			User thisUser = (User) req.getSession().getAttribute("user");
 			
-			if (req.getMethod() == "POST") {
-
-				User u = (User) om.readValue(req.getInputStream(), User.class);
+			System.out.println(thisUser);
+			
+			if ("POST".equals(req.getMethod())) {
 					
-				Role r = u.getRole();
+				System.out.println(thisUser.getRole().getRole().toString());
+				
+				if ("Admin".equals(thisUser.getRole().getRole().toString())) {
+						
+					User u = (User) om.readValue(req.getInputStream(), User.class);					
+						
+						u.setUserId(aServ.registerUser(u));
 					
-				if (r.toString() == "Admin") {
-						
-					u.setUserId(aServ.registerUser(u));
-					resp.getWriter().write(om.writeValueAsString(u));
-					resp.setStatus(HttpServletResponse.SC_CREATED);
-						
+						resp.getWriter().write(om.writeValueAsString(u));
+						resp.setStatus(HttpServletResponse.SC_CREATED);
+					
 				} else {
 					
-					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 				}
 					
 			} else {
 					
 				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 				
-			}			
+			}
+			
 		}
+		
 	}
+	
 }
