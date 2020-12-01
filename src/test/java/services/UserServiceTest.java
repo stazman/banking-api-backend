ef3e427697945313655fbf4f90624d18a50abd07
Christopher DiStasio <cadistasio@gmail.com>
2020-12-01 10:28:57 -0600

    Uses slf4j to output parameterized message

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -8,14 +8,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserServiceTest {
 
     UserDAO ud;
     UserService us;
 
-    private final Logger logger = Logger.getLogger(String.valueOf(UserServiceTest.class));
+//    private final Logger logger = Logger.getLogger(String.valueOf(UserServiceTest.class));
+    private final Logger logger = LoggerFactory.getLogger(String.valueOf(UserServiceTest.class));
+
+    String findById = "testFindUserById()";
 
     @Before
     public void testSetup(){
@@ -27,11 +32,11 @@ public class UserServiceTest {
     public void testFindUserById() {
 
         Role r = new Role(1, "Admin");
-        User u = new User(101, "aa", "aa", "aa", "aa", "aa@a.com", r);
+        User u = new User(230, "aa", "aa", "aa", "aa", "aa@a.com", r);
 
-        Mockito.when(ud.getUserById(101)).thenReturn(u);
+        Mockito.when(ud.getUserById(230)).thenReturn(u);
 
-        logger.info("The findUserById() service method test has been run.");
+        logger.info("The {} service method test has been run.", findById);
     }
 }
 
